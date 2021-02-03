c6804e6f0c622a71624d7c2831feb9095e718a1c
Po-Yuan Wang <andrew.wang@onedegree.hk>
2021-02-03 20:51:03 +0800

    add 3 sum advanced solution

--- 015_3Sum/3sum.py
+++ 015_3Sum/3sum.py
@@ -3,7 +3,7 @@ class ThreeSum():
     def __init__(self):
         pass
     
-    def method_1(self, nums, target):
+    def method_basic(self, nums, target):
 
         result = []
         i = 0
@@ -16,13 +16,43 @@ class ThreeSum():
                     if nums[i] + nums[j] + nums[k] == 0:                      
                         temp = [nums[i], nums[j], nums[k]]
                         temp.sort()
-                        
+
                         if temp not in result:
                             result.append(temp)
                     k+=1
                 j+=1
             i+=1
         
+        return result
+
+    def method_advance(self, nums, target):
+
+        result = []
+        nums.sort()
+
+        for first in range(len(nums)-2):
+
+            second = first + 1
+            third = len(nums) - 1
+
+            while second < third:
+
+                if nums[first] + nums[second] + nums[third] == 0:
+                    temp = [nums[first], nums[second], nums[third]]
+                    temp.sort()
+                    if temp not in result:
+                        result.append(temp)
+
+                    second += 1
+                    third -= 1
+
+                elif nums[second] + nums[third] < nums[first] * -1:
+                    second += 1
+                elif nums[second] + nums[third] > nums[first] * -1:
+                    third -= 1 
+                else:
+                    pass
+
         return result
 
 
@@ -33,5 +63,6 @@ if "__main__" == __name__:
     target = 0
 
     test = ThreeSum()
-    result = test.method_1(nums=nums, target=target)
+    # result = test.method_basic(nums=nums, target=target)
+    result = test.method_advance(nums=nums, target=target)
     print(result)
